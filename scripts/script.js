251dceb51543a376f2ceef0ea4e59c7e26ad92cd
Usuario Sala dos Professores <profjnpaulista@sereduc.intra>
2024-03-28 12:06:21 -0300

    update imagens

--- scripts/script.js
+++ scripts/script.js
@@ -27,11 +27,12 @@ function getCidades() {
       let divResults = document.createElement("div");
 
       divResults.innerHTML = `   
-     <button class="flex w-[100%] gap-[5px] my-[5px] py-[10px] outline-none hover:bg-[#F2F2F2] pl-[10px] rounded-[3px]" onclick="getClimas('${element.name}', ${element.lat}, ${element.lon})">
+      <button class="flex w-[100%] gap-[5px] my-[5px] py-[10px] outline-none hover:bg-[#F2F2F2] pl-[10px] rounded-[3px]"  onclick="getClimas('${element.name}', ${element.lat}, ${element.lon}, '${element.admin1}', '${element.country}')">
         <p>${element.name}, </p>
         <p>${element.admin1} - </p>
         <p>${element.country}</p>
-     </button>
+       </button>
+  
         `;
 
       divResultados.appendChild(divResults);
@@ -43,7 +44,7 @@ function getCidades() {
 
 document.getElementById("campoClima").addEventListener("keyup", getCidades);
 
-function getClimas(nomeCidade, latitude, longitude) {
+function getClimas(nomeCidade, latitude, longitude, nomeEstado, pais) {
   fetch(
     `https://my.meteoblue.com/packages/current?&name=${nomeCidade}&lat=${latitude}&lon=${longitude}&apikey=73WC9IzLXcoNfuPw 
     `
@@ -64,14 +65,8 @@ function getClimas(nomeCidade, latitude, longitude) {
     let infoMinutes = time.getMinutes(); // minutos
     let minutes = infoMinutes < 10 ? `0${infoMinutes}` : infoMinutes; // minutos atualizados
 
-    console.log(isDayLight);
-    console.log(pictocode); // condicoes climaticas
-    console.log(temperature); // temperatura
-    console.log(time); // data e horário
-    console.log(infoHour);
-    console.log(minutes);
-    console.log(infoDay); // dia da semana
 
+    document.getElementById('country').innerText = `${nomeCidade}, ${nomeEstado} - ${pais}`
     document.getElementById("temperatura").innerText = temperature;
     document.getElementById("graus").classList.remove("hidden");
 
@@ -79,66 +74,83 @@ function getClimas(nomeCidade, latitude, longitude) {
       case 1:
         document.getElementById("pictograma").innerText =
           "Céu claro e sem nuvens";
+          document.getElementById('imgClima').src="public/claroSemNuvens.png"
         break;
       case 2:
         document.getElementById("pictograma").innerText =
           "Claro e poucas nuvens";
-        break;
+          document.getElementById('imgClima').src="public/claro e poucas nuvens.png"
+          break;
       case 3:
         document.getElementById("pictograma").innerText =
           "Parcialmente nublado";
+        document.getElementById('imgClima').src="public/parcialmente nublado.png"
         break;
       case 4:
         document.getElementById("pictograma").innerText = "Nublado";
+        document.getElementById('imgClima').src="public/nublado.png"
         break;
       case 5:
         document.getElementById("pictograma").innerText = "Névoa";
+        document.getElementById('imgClima').src="public/nublado.png"
         break;
       case 6:
         document.getElementById("pictograma").innerText = "Nublado com chuva";
+        document.getElementById('imgClima').src="public/nubladoComChuva.png"
         break;
       case 7:
         document.getElementById("pictograma").innerText =
           "Misturado com chuvas";
+        document.getElementById('imgClima').src="public/MisturadoComchuvas.png"
         break;
       case 8:
         document.getElementById("pictograma").innerText =
           "Possíveis pancadas de chuva e trovoadas";
+        document.getElementById('imgClima').src="public/possiveisPancadasDeChuvaEtrovoadas.png"
         break;
       case 9:
         document.getElementById("pictograma").innerText = "Nublado com neve";
+        document.getElementById('imgClima').src="public/claro e poucas nuvens.png"
         break;
       case 10:
         document.getElementById("pictograma").innerText =
           "Misturado com pancadas de neve";
+        document.getElementById('imgClima').src="public/claro e poucas nuvens.png"
         break;
       case 11:
         document.getElementById("pictograma").innerText =
           "Muito nublado com uma mistura de neve e chuva";
+        document.getElementById('imgClima').src="public/claro e poucas nuvens.png"
         break;
       case 12:
         document.getElementById("pictograma").innerText =
           "Nublado com chuva fraca";
+        document.getElementById('imgClima').src="public/claro e poucas nuvens.png"
         break;
       case 13:
         document.getElementById("pictograma").innerText =
           "Nublado com neve fraca";
+        document.getElementById('imgClima').src="public/claro e poucas nuvens.png"
         break;
       case 14:
         document.getElementById("pictograma").innerText =
           "Parcialmente nublado com chuva";
+        document.getElementById('imgClima').src="public/claro e poucas nuvens.png"
         break;
       case 15:
         document.getElementById("pictograma").innerText =
           "Maioritariamente nublado com neve";
+        document.getElementById('imgClima').src="public/claro e poucas nuvens.png"
         break;
       case 16:
         document.getElementById("pictograma").innerText =
           "Muito nublado com chuva fraca";
+        document.getElementById('imgClima').src="public/claro e poucas nuvens.png"
         break;
       case 17:
         document.getElementById("pictograma").innerText =
           "Parcialmente nublado com neve fraca";
+        document.getElementById('imgClima').src="public/claro e poucas nuvens.png"
         break;
     }
 
